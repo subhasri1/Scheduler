944791d3a4983ef771babfeda96ff61c7758d89c
SahilBalyan1122 <96620003+SahilBalyan1122@users.noreply.github.com>
2022-04-27 23:51:39 +0530

    Add files via upload

--- class.h
+++ class.h
@@ -1,21 +1,33 @@
-#include<stdio.h>
-#include<iostream>
+//#include<stdio.h>
+//#include<iostream>
 
-using namespace std;
+//using namespace std;
 
 class details{
 public:
-//string name;
-int rollno,standard,fee;
-void set(int r,int s,int f){
-rollno=r;
-standard=s;
-fee=f;
+int Pid,ArrivalTime,BurstTime,ProcessType,Priority,TimeQuantum;
+void set(int r,int s,int f,int t,int p,int q){
+Pid=r;
+ArrivalTime=s;
+BurstTime=f;
+ProcessType=t;
+Priority=p;
+TimeQuantum=q;
 };
 void get(){
-cout<<rollno<<endl;
-cout<<standard<<endl;
-cout<<fee<<endl;
-//cout<<name<<endl;
 };
 };
+
+struct process{
+int pid,processtype,BurstTime,ArrivalTime,Priority,TimeQuantum,WaitingTime,TurnAroundTime,ComplitionTime,bt_remaining,StartTime,ResponseTime;
+};
+
+struct process* fcfs();
+struct process* priority();
+struct process* rr();
+
+
+
+
+
+
