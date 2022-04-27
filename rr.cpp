c3e339b3b77f3a1db4a8e1a7c24396ba197b7b60
Aman Deo <101329619+aman-capg@users.noreply.github.com>
2022-04-27 13:00:49 +0530

    Add files via upload

--- rr.cpp
+++ rr.cpp
@@ -1,4 +1,4 @@
-// Program to implement Round Robin CPU Scheduling Algorithm
+// Round Robin CPU Scheduling Algorithm
 #include <iostream>
 #include <algorithm>
 #include <queue>
@@ -6,20 +6,24 @@
 using namespace std;
 
 //We save all the process data into a structure
-struct process_struct
-{
-    int pid;
-    int at;
-    int bt;
-    int ct, wt, tat, rt, start_time;
-    int bt_remaining;
-} ps[100];
+// struct process_struct
+// {
+//     int pid;
+//     int at; //ArrivalTime
+//     int bt; //BurstTime
+//     int ct, wt, tat, rt, start_time; //WaitingTime //TurnAroundTime //TimeQuantum
+//     int bt_remaining;
+// } ps[100]; //process_struct to process
+
+struct process{
+int pid,processtype,BurstTime,ArrivalTime,Priority,TimeQuantum,WaitingTime,TurnAroundTime,ComplitionTime,StartTime, bt_remaining, ResponseTime;
+}ps[100]; //SAHIL BHAI ADD StartTime, bt_remaining, ResponseTime, sum_tat = 0, sum_wt=0, sum_rt=0
 
 //This is to compare processes on the basis of Arrival Time
-bool compareAT(struct process_struct a, struct process_struct b) // arrival time compare karwane ke liye
+bool compareAT(struct process a, struct process b) // arrival time compare karwane ke liye
 {
-    int x = a.at;
-    int y = b.at;
+    int x = a.ArrivalTime;
+    int y = b.ArrivalTime;
     return x < y;
     //    if(x > y)
     //    return false;  // Apply sorting
@@ -27,7 +31,7 @@ bool compareAT(struct process_struct a, struct process_struct b) // arrival time
 }
 
 //This is to compare process ID of the processes
-bool comparePID(struct process_struct a, struct process_struct b)
+bool comparePID(struct process a, struct process b)
 {
     int x = a.pid;
     int y = b.pid;
@@ -41,7 +45,7 @@ int main()
     queue<int> q;
     bool visited[100] = {false}, is_first_process = true;
     int current_time = 0, max_completion_time;                // current time set to 0 at the beginning
-    int completed = 0, tq, total_idle_time = 0; 	      // completed also set to 0 at the beginning
+    int completed = 0, TimeQuantum, total_idle_time = 0; 	      // completed also set to 0 at the beginning
     cout << "Enter total number of processes: ";
     cin >> n;
     float sum_tat = 0, sum_wt = 0, sum_rt = 0;
@@ -51,19 +55,19 @@ int main()
     for (int i = 0; i < n; i++) // taking input of Arrival time of the processes
     {
         cout << "\nEnter Process " << i << " Arrival Time: ";
-        cin >> ps[i].at; // ps[i].at is arrival time
+        cin >> ps[i].ArrivalTime; // ps[i].ArrivalTime is arrival time
         ps[i].pid = i;
     }
 
     for (int i = 0; i < n; i++) // Taking input of burst time of the processes
     {
         cout << "\nEnter Process " << i << " Burst Time: ";
-        cin >> ps[i].bt;
-        ps[i].bt_remaining = ps[i].bt;
+        cin >> ps[i].BurstTime;
+        ps[i].bt_remaining = ps[i].BurstTime;
     }
 
     cout << "\nEnter time slice: "; // Taking input of the time slice
-    cin >> tq;
+    cin >> TimeQuantum;
 
     // sort structure on the basis of Arrival time in increasing order
     sort(ps, ps + n, compareAT);
@@ -78,20 +82,20 @@ int main()
         index = q.front(); // push the index in front of the queue
         q.pop();
 
-        if (ps[index].bt_remaining == ps[index].bt) // process went in for the first time
+        if (ps[index].bt_remaining == ps[index].BurstTime) // process went in for the first time
         {
             // taking maximum of current time and arrival time, giving that value in start time
-            ps[index].start_time = max(current_time, ps[index].at);
-            total_idle_time += (is_first_process == true) ? 0 : ps[index].start_time - current_time;
+            ps[index].StartTime = max(current_time, ps[index].ArrivalTime);
+            total_idle_time += (is_first_process == true) ? 0 : ps[index].StartTime - current_time;
             // assigning the current time as start time for current process
-            current_time = ps[index].start_time;
+            current_time = ps[index].StartTime;
             is_first_process = false;
         }
 
-        if (ps[index].bt_remaining - tq > 0) // if remaining burst Time - time slice is more than 0
+        if (ps[index].bt_remaining - TimeQuantum > 0) // if remaining burst Time - time slice is more than 0
         {
-            ps[index].bt_remaining -= tq; // remaining burst time = remaining burst time - time slice
-            current_time += tq;           // current time will be changed to current time + time slice
+            ps[index].bt_remaining -= TimeQuantum; // remaining burst time = remaining burst time - time slice
+            current_time += TimeQuantum;           // current time will be changed to current time + time slice
         }
         else // If remaining burst - time slice is 0 (Means the process no longer needs to be executed)
         {
@@ -99,20 +103,20 @@ int main()
             ps[index].bt_remaining = 0; // BT remaining will be set to 0
             completed++;                // and the process will be completed
 
-            ps[index].ct = current_time;
-            ps[index].tat = ps[index].ct - ps[index].at;
-            ps[index].wt = ps[index].tat - ps[index].bt;
-            ps[index].rt = ps[index].start_time - ps[index].at;
+            ps[index].ComplitionTime = current_time;
+            ps[index].TurnAroundTime = ps[index].ComplitionTime - ps[index].ArrivalTime;
+            ps[index].WaitingTime = ps[index].TurnAroundTime - ps[index].BurstTime;
+            ps[index].ResponseTime = ps[index].StartTime - ps[index].ArrivalTime;
 
-            sum_tat += ps[index].tat;
-            sum_wt += ps[index].wt;
-            sum_rt += ps[index].rt;
+            sum_tat += ps[index].TurnAroundTime;
+            sum_wt += ps[index].WaitingTime;
+            sum_rt += ps[index].ResponseTime;
         }
 
         // check which new process needs to be pushed to Ready Queue from Input list
         for (int i = 1; i < n; i++)
         {
-            if (ps[i].bt_remaining > 0 && ps[i].at <= current_time && visited[i] == false)
+            if (ps[i].bt_remaining > 0 && ps[i].ArrivalTime <= current_time && visited[i] == false)
             {
                 q.push(i);
                 visited[i] = true;
@@ -139,16 +143,16 @@ int main()
 
     // Calculate Length of Process completion cycle
     for (int i = 0; i < n; i++)
-        max_completion_time = max(max_completion_time, ps[i].ct);
+        max_completion_time = max(max_completion_time, ps[i].ComplitionTime);
 
 
     // sort so that process ID in output comes in Original input order
     sort(ps, ps + n, comparePID);
 
-    // Output of alll the calculations
+    // Output of all the calculations
     cout << "\nProcess No.\tAT\tBurst Time\tStart Time\tCT\tTAT\tWT\tRT\n";
     for (int i = 0; i < n; i++)
-        cout << i << "\t\t" << ps[i].at << "\t" << ps[i].bt << "\t\t" << ps[i].start_time << "\t\t" << ps[i].ct << "\t" << ps[i].tat << "\t" << ps[i].wt << "\t" << ps[i].rt << endl;
+        cout << i << "\t\t" << ps[i].ArrivalTime << "\t" << ps[i].BurstTime << "\t\t" << ps[i].StartTime << "\t\t" << ps[i].ComplitionTime << "\t" << ps[i].TurnAroundTime << "\t" << ps[i].WaitingTime << "\t" << ps[i].ResponseTime << endl;
     cout << endl;
 
     cout << "\nAverage Turn Around time= " << (float)sum_tat / n;       // may not be needed
