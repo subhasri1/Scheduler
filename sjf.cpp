c31101b86c28b2012bf8719a32f812fa127c7af4
Shaikh Gulam Mohiuddin <buntyshaikh1998@gmail.com>
2022-04-29 17:38:24 +0530

    Add files via upload

--- sjf.cpp
+++ sjf.cpp
@@ -14,17 +14,16 @@ using namespace std;
 #include <iomanip>
 struct process* sjf()
 {
-//int j,BurstTime[20],WaitingTime[20],TurnAroundTime[20],avwt=0,avtArrivalTime=0,i,j,arrival[20],ComplitionTime[20],ResponseTime[20];
 string st,BurstTime,pid,ArrivalTime;
 int flag=0,k,j=0;
 fstream file1;
 file1.open("dt.txt",ios::in);
-//struct process* p = (process*)calloc(10,sizeof(struct process));
-//struct process p[10];
+
 struct process *p=new struct process[10];
-//struct process *p;
-while(getline(file1,st)){
-//cout<<st<<endl;
+
+while(getline(file1,st))
+{
+
 for(int i=0;i<st.size();i++)
 {
 
@@ -52,7 +51,7 @@ BurstTime+=st[i];
 
 }
 }
-//cout<<"pid:"<<pid;
+
 p[j].pid=stoi(pid);
 p[j].ArrivalTime=stoi(ArrivalTime);
 p[j].BurstTime=stoi(BurstTime);
@@ -63,39 +62,31 @@ j++;
 flag=0;
 }
 file1.close();
-/*
-struct process_struct
-{
-  int Pid;
-  int ArrivalTime;
-  int BurstTime;
-  int ComplitionTime,WaitingTime,TurnAroundTime,ResponseTime,St;
-}p[100];*/
 
     bool is_completed[100]={false},is_first_process=true;
     int current_time = 0;
     int completed = 0;;
-    //cout<<"Enter total number of processes: ";
-    //cin>>j;    
+    cout<<"Enter total number of processes: ";
+    
     int sum_TurnAroundTime=0,sum_WaitingTime=0,sum_ResponseTime=0,total_idle_time=0,prev=0,length_cycle;
     float cpu_utilization;
     int max_ComplitionTime,min_ArrivalTime;
     
-    //cout << fixed << setprecision(2);
+    cout << fixed << setprecision(2);
 
-    /*for(int i=0;i<j;i++)
+    for(int i=0;i<j;i++)
     {
         cout<<"\nEnter Process " <<i<< " Arrival Time: ";
-        //cin >> p[i].ArrivalTime;
+       
         p[i].pid=i;
     }
     
     for(int i=0;i<j;i++)
     {
         cout<<"\nEnter Process " <<i<< " Burst Time: ";
-        //cin >> p[i].BurstTime;
+  
     }
-    */
+    
     
     while(completed!=j)
     {
@@ -128,7 +119,7 @@ struct process_struct
         p[min_index].TurnAroundTime = p[min_index].ComplitionTime - p[min_index].ArrivalTime;
         p[min_index].WaitingTime = p[min_index].TurnAroundTime - p[min_index].BurstTime;
         p[min_index].ResponseTime = p[min_index].WaitingTime;
-        // p[min_index].ResponseTime = p[min_index].St - p[min_index].ArrivalTime;
+    
                 
         sum_TurnAroundTime +=p[min_index].TurnAroundTime;
         sum_WaitingTime += p[min_index].WaitingTime;
@@ -154,17 +145,18 @@ struct process_struct
     length_cycle = max_ComplitionTime - min_ArrivalTime;
 
      //Output
-    //cout<<"\nProcess No.\tTurnAroundTime\tCPU Burst Time\tCT\tTArrivalTime\tWT\tRT\n";
+    cout<<"\nProcess No.\tTurnAroundTime\tCPU Burst Time\tCT\tTArrivalTime\tWT\tRT\n";
     for(int i=0;i<j;i++)
-     //cout<<i<<"\t\t"<<p[i].ArrivalTime<<"\t"<<p[i].BurstTime<<"\t\t"<<p[i].ComplitionTime<<"\t"<<p[i].TurnAroundTime<<"\t"<<p[i].WaitingTime<<"\t"<<p[i].ResponseTime<<endl;
-    //cout<<endl;  
+     cout<<i<<"\t\t"<<p[i].ArrivalTime<<"\t"<<p[i].BurstTime<<"\t\t"<<p[i].ComplitionTime<<"\t"<<p[i].TurnAroundTime<<"\t"<<p[i].WaitingTime<<"\t"<<p[i].ResponseTime<<endl;
+    cout<<endl;  
     
     cpu_utilization = (float)(length_cycle - total_idle_time)/ length_cycle;
 
-    /*cout<<"\nAverage Turn Around time= "<< (float)sum_TurnAroundTime/j;
+    cout<<"\nAverage Turn Around time= "<< (float)sum_TurnAroundTime/j;
     cout<<"\nAverage Waiting Time= "<<(float)sum_WaitingTime/j;
     cout<<"\nAverage Response Time= "<<(float)sum_ResponseTime/j;    
     cout<<"\nThroughput= "<<j/(float)length_cycle;
-    cout<<"\nCPU Utilization(Percentage)= " << cpu_utilization*100;*/
+    cout<<"\nCPU Utilization(Percentage)= " << cpu_utilization*100;
     return p;
 }
+
