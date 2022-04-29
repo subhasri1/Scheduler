f3374943789b589d1fef8ea430b35ce6bcceef9a
Nitish-Mahato <103922375+Nitish-Mahato@users.noreply.github.com>
2022-04-29 15:12:53 +0530

    Add files via upload

--- fcfs.cpp
+++ fcfs.cpp
@@ -13,17 +13,15 @@ using namespace std;
 #include <iomanip>
 
 
-bool comparisonAT(struct process a, struct process b)
+bool comparisonAT(struct process a, struct process b)//Compare Arrival Time
 {
     int x = a.ArrivalTime;
     int y = b.ArrivalTime;
     return x < y;
-    //    if(x > y)
-    //      return false;  // Apply sorting
-    //    return true;   // no sorting
+
 }
 
-bool comparisonPID(struct process a, struct process b)
+bool comparisonPID(struct process a, struct process b)//Compare Pid
 {
     int x = a.pid;
     int y = b.pid;
@@ -31,19 +29,18 @@ bool comparisonPID(struct process a, struct process b)
 }
 
 
-struct process* fcfs()
+struct process* fcfs()//Structure contains FCFS scheduler
 {
-//int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j,arrival[20],ct[20],rt[20];
+
 string st,BurstTime,pid,ArrivalTime;
 int flag=0,k,j=0;
 fstream file1;
 file1.open("dt.txt",ios::in);
-//struct process* p = (process*)calloc(10,sizeof(struct process));
-//struct process p[10];
+//hear we are opening the file dt.txt in read mode
 struct process *p=new struct process[10];
-//struct process *p;
+//we are fetching each line of dt.txt file for storing Arrival Time Brust Time
 while(getline(file1,st)){
-//cout<<st<<endl;
+
 for(int i=0;i<st.size();i++)
 {
 
@@ -71,7 +68,7 @@ BurstTime+=st[i];
 
 }
 }
-//cout<<"pid:"<<pid;
+//value for pid arrival time Brust time in structure process define in class.h file
 p[j].pid=stoi(pid);
 p[j].ArrivalTime=stoi(ArrivalTime);
 p[j].BurstTime=stoi(BurstTime);
@@ -81,50 +78,21 @@ BurstTime.clear();
 j++;
 flag=0;
 }
-//cout<<p[0].pid<<"mila"<<endl;
+
 file1.close();
+//close
 
 
 
-// fcfs();
-//process p[10];
-//cout<<p[0].pid<<"jeet"<<endl;
-//write(client_fd,&p,sizeof(p));
-
-
-//using namespace std;
-/*struct process_struct
-{
-    int pid;
-    int at;                          // Arrival Time
-    int bt;                          // CPU Burst time
-    int ct, wt, tat, rt, start_time; // Completion, waiting, turnaround, response time
-} ps[100];                           // Array of structure to store the info of each process.
-*/
-//struct process p[100];
 
 
     float sum_tat = 0, sum_wt = 0, sum_rt = 0;
     int length_cycle, total_idle_time = 0;
 
-    //cout << fixed << setprecision(2);
-    /*for (int i = 0; i < n; i++)
-    {
-        cout << "\nEnter Process " << i << " Arrival Time: ";
-        cin >> ps[i].ArrivalTime;
-        ps[i].pid = i;
-    }
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << "\nEnter Process " << i << " Burst Time: ";
-       // cin >> ps[i].BurstTime;
-    }
-*/
-    // sort
-    sort(p, p + j, comparisonAT);
+    sort(p, p + j, comparisonAT);//sorting of Arrival time
 
-    // calculation
+
     for (int i = 0; i < j; i++)
     {
         p[i].StartTime = (i == 0) ? p[i].ArrivalTime : max(p[i].ArrivalTime, p[i - 1].ComplitionTime);
@@ -132,69 +100,16 @@ file1.close();
         p[i].TurnAroundTime = p[i].ComplitionTime - p[i].ArrivalTime;
         p[i].WaitingTime = p[i].TurnAroundTime - p[i].BurstTime;
         p[i].ResponseTime = p[i].StartTime - p[i].ArrivalTime;
-
+//Calculation Of Complition Time, Turn Around Time, Waiting time, Response Time.
         sum_tat += p[i].TurnAroundTime;
         sum_wt += p[i].WaitingTime;
         sum_rt += p[i].ResponseTime;
         total_idle_time += (i == 0) ? 0 : (p[i].StartTime - p[i - 1].ComplitionTime);
     }
     length_cycle = p[j - 1].ComplitionTime - p[0].StartTime;
-    // sort so that process ID in output comes in Original order (just for interactivity)
-    sort(p, p + j, comparisonPID);
 
-    // Output
-    //cout << "\nProcess No.\tAT\tCPU Burst Time\tCT\tTAT\tWT\tRT\n";
-    /*for (int i = 0; i < j; i++)
-        cout << i << "\t\t" << p[i].ArrivalTime << "\t" << p[i].BurstTime << "\t\t" << p[i].ComplitionTime << "\t" << p[i].TurnAroundTime << "\t" << p[i].WaitingTime << "\t" << p[i].ResponseTime << endl;
-    cout << endl;*/
+    sort(p, p + j, comparisonPID);//Sorting Of pid
 
-    //cout << "\nAverage Turn Around time= " << sum_tat / j;
-    //cout << "\nAverage Waiting Time= " << sum_wt / j;
-    //cout << "\nAverage Response Time= " << sum_rt / j;
+
     return p;
 }
-/*
-    cout<<"Enter total number of processes(maximum 20):";
-    cin>>n;
-
-
-    cout<<"\nEnter Process Arrival Time aka DURATION \n";
-    for(i=0;i<n;i++)
-    {
-        cout<<"P["<<i+1<<"]:";
-        cin>>arrival[i];
-    }
-
-    cout<<"\nEnter Process Burst Time aka DURATION \n";
-    for(i=0;i<n;i++)
-    {
-        cout<<"P["<<i+1<<"]:";
-        cin>>bt[i];
-    }
-
-
-    wt[0]=0;    //waiting time for first process is 0
-    //calculating waiting time
-    for(i=1;i<n;i++)
-    {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
-    }
-    cout<<"\nProcess\tArrival Time \tBurst Time\tWaiting Time\tTurnaround Time \tCompletion Time \tResponse Time";
-    //calculating turnaround time
-    for(i=0;i<n;i++)
-    {
-        tat[i]=bt[i]+wt[i];
-        avwt+=wt[i];
-        avtat+=tat[i];
-        ct[i]=arrival[i]+tat[i];
-
-     cout<<"\nP["<<i+1<<"]" "\t\t" <<arrival[i]<<"\t\t"<<bt[i]<<"\t\t"<<wt[i]<<"\t\t"<<tat[i]<<"\t\t"<<ct[i];
-    }
-    avwt/=i;
-    avtat/=i;
-    cout<<"\n\nAverage Waiting Time:"<<avwt;
-    cout<<"\nAverage Turnaround Time:"<<avtat;
-    */
-    //cout<<"pid:"<<p[0].pid;
